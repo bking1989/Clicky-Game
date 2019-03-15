a619cbebfdb092a2aaefb07bd2e759f86a2e2b97
bking1989 <bradley_king@me.com>
2019-03-14 17:00:43 -0700

    Added header component and got React working

---
+++ clicky-game/src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <a className="navbar-brand text-white">Clicky Game!</a>
+        </nav>
+    );
+}
+
+export default Header;
