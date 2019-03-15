2574c75e2a8367b05d0b33bc2696dcfd34e896e3
bking1989 <bradley_king@me.com>
2019-03-15 15:55:35 -0700

    Set up class for header, complete with scores and previous click

--- clicky-game/src/components/header.js
+++ clicky-game/src/components/header.js
@@ -1,12 +1,24 @@
 import React from "react";
 
-function Header() {
-    return (
-        <nav className="navbar navbar-expand-lg bg-dark">
-        {/* eslint-disable-next-line */}
-            <a className="navbar-brand text-white">Clicky Game!</a>
-        </nav>
-    );
+class Header extends React.Component {
+    state = {
+        previousValue: "",
+        highScore: 0,
+        currentScore: 0,
+    }
+
+    render () {
+        return (
+            <nav className="navbar navbar-expand-lg bg-dark">
+            {/* eslint-disable-next-line */}
+                <a className="navbar-brand navbar-nav text-white">Clicky Game!</a>
+                <ul className="navbar-nav text-white ml-auto d-inline text-right">
+                    <li classname="list-item"><b>Current Score:</b> {this.state.currentScore}</li>
+                    <li classname="list-item"><b>High Score:</b> {this.state.highScore}</li>
+                </ul>
+            </nav>
+        );
+    }
 }
 
 export default Header;
