c3be6c7a688a9e604e8931632d957e620a1ca7c2
bking1989 <bradley_king@me.com>
2019-03-15 15:32:38 -0700

    Made it so that components recieved random image and prop on refresh

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -4,29 +4,33 @@ import Wrapper from "./components/wrapper";
 import Row from "./components/row";
 import Card from "./components/card";
 
+const charArr = ["Ned Stark", "Arya Stark", "Sansa Stark", "Jon Snow", "Joffrey Baratheon", "Cersei Lannister", "Tyrion Lannister", "Petyr Baelish", "Daenarys Targaryen"];
+
 function App() {
+    let shuffleArr = charArr.sort(() => {return 0.5 - Math.random()});
+
     return (
         <div>
             <Header />
             <Wrapper>
                 <Row>
-                    <Card value="Ned Stark" />
-                    <Card value="Robert Baratheon" />
-                    <Card value="Cersei Lannister" />
+                    <Card value={shuffleArr[0]} />
+                    <Card value={shuffleArr[1]} />
+                    <Card value={shuffleArr[2]} />
                 </Row>
                 <Row>
-                    <Card value="Jon Snow" />
-                    <Card value="Arya Stark" />
-                    <Card value="Sansa Stark" />
+                    <Card value={shuffleArr[3]} />
+                    <Card value={shuffleArr[4]} />
+                    <Card value={shuffleArr[5]} />
                 </Row>
                 <Row>
-                    <Card value="Joffrey Baratheon" />
-                    <Card value="Jaime Lannister" />
-                    <Card value="Petyr Balish" />
+                    <Card value={shuffleArr[6]} />
+                    <Card value={shuffleArr[7]} />
+                    <Card value={shuffleArr[8]} />
                 </Row>
             </Wrapper>
         </div>
     );
-}
+};
 
 export default App;
--- clicky-game/src/components/header.js
+++ clicky-game/src/components/header.js
@@ -3,6 +3,7 @@ import React from "react";
 function Header() {
     return (
         <nav className="navbar navbar-expand-lg bg-dark">
+        {/* eslint-disable-next-line */}
             <a className="navbar-brand text-white">Clicky Game!</a>
         </nav>
     );
