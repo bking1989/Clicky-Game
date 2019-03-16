9058157b013c569ca74d1d9dc057a67ca3419e65
bking1989 <bradley_king@me.com>
2019-03-16 12:48:45 -0700

    Made slight style update

--- clicky-game/src/components/header.js
+++ clicky-game/src/components/header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
             <nav className="navbar navbar-expand-lg bg-dark">
                 <ul className="navbar-nav w-100 text-white d-flex justify-content-around">
                     <li className="nav-item"><b>Current Score:</b> {this.props.currentScore}</li>
-                    <li className="nav-item"><h5>Clicky Game!</h5></li>
+                    <li className="nav-item"><h3>Clicky Game!</h3></li>
                     <li className="nav-item"><b>High Score:</b> {this.props.highScore}</li>
                 </ul>
             </nav>
