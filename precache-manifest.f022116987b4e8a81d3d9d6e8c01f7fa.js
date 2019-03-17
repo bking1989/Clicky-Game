592dce99e50b815d8415a90d673b7b3213ff71cf
bking1989 <bradley_king@me.com>
2019-03-17 15:37:32 -0700

    Updates

---
+++ precache-manifest.f022116987b4e8a81d3d9d6e8c01f7fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "5a8a70bfc7091ccf2cf8",
+    "url": "/Clicky-Game/static/js/main.f058475c.chunk.js"
+  },
+  {
+    "revision": "f36054a2f6c5bf348fa1",
+    "url": "/Clicky-Game/static/js/2.4214afab.chunk.js"
+  },
+  {
+    "revision": "5a8a70bfc7091ccf2cf8",
+    "url": "/Clicky-Game/static/css/main.bf5c5f8a.chunk.css"
+  },
+  {
+    "revision": "3140f4be8b716eebd58e0fa18800f708",
+    "url": "/Clicky-Game/index.html"
+  }
+];
